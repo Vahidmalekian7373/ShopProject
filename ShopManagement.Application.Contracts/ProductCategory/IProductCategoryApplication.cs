2a5ac190ce1766efb763d7315e10974f08dfc5e6
Malekian_Vahid <vahidmalekian7373@gmail.com>
2022-02-07 23:55:26 +0330

    create ProductCategory and Repository

---
+++ ShopManagement.Application.Contracts/ProductCategory/IProductCategoryApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManagement.Application.Contracts.ProductCategory
+{
+    public interface IProductCategoryApplication
+    {
+        void Create(CreateProductCategory command);
+        void Edit(EditProductCategory command);
+        Domain.ProductCategoryAgg.ProductCategory  GetDetails(long id);
+        List<ProductCategoryViewModel> Serach(ProductCategorySearchModel searchModel);
+
+    } 
+}
--- ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
+++ ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _0_Framework.Domain
+namespace ShopManagement.Domain.ProductCategoryAgg
 {
     public interface IProductCategoryRepository
     {
+        void Create(ProductCategory entity);
+        ProductCategory Get(long id);
+        List<ProductCategory> GetAll();
     }
 }
